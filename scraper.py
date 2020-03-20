484ab08a91f9add393bfab14e75f04f8b6248a07
Eduardo Rodrigues <44787022+Eduaaado@users.noreply.github.com>
2020-03-20 13:12:52 -0300

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+driver = webdriver.Chrome(executable_path = 'tool/chromedriver')
+driver.get("https://www.indeed.com.br/")
+time.sleep(1)
+
+what = driver.find_element_by_id('text-input-what')
+what.send_keys('python')
+
+where = driver.find_element_by_id('text-input-where')
+where.send_keys('São Paulo')
+where.submit()
+time.sleep(1)
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+results = soup.findAll('a', {'class': 'jobtitle'})
+
+jobs = driver.find_elements_by_class_name('jobtitle')
+jobtitles = [job.get_attribute("innerHTML")[1:] for job in jobs]
+print(jobtitles)
